218ea1c204a4cef4880f260867e8906a33b940cf
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-04-01 00:58:40 +0430

    multiple images To Pdf method added

--- src/main/java/PdfConverter.java
+++ src/main/java/PdfConverter.java
@@ -4,13 +4,18 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Image;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.util.List;
+
 
 public class PdfConverter {
 
-    public static final String DEST = "./target/test/resources/sandbox/tables/imagesToPdf.pdf";
-    public static final String IMG1 = "./src/main/resources/img/f.jpg";
-    public static final String IMG2 = "./src/main/resources/img/s.jpg";
+    private static final String DEST = "./target/test/resources/sandbox/tables/imagesToPdf.pdf";
+    private static final String IMG1 = "./src/main/resources/img/f.jpg";
+    private static final String IMG2 = "./src/main/resources/img/s.jpg";
 
     public static void main(String[] args) throws Exception {
         File file = new File(DEST);
@@ -18,14 +23,49 @@ public class PdfConverter {
         new PdfConverter().manipulatePdf(DEST);
     }
 
-    public void manipulatePdf(String dest) throws Exception {
-        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
-        Document doc = new Document(pdfDoc);
+    public void manipulatePdf(String dest) {
 
-        doc.add(new Image(ImageDataFactory.create(IMG1)));
-        doc.add(new Image(ImageDataFactory.create(IMG2)));
+        try (PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+             Document doc = new Document(pdfDoc)) {
 
-        doc.close();
+            doc.add(new Image(ImageDataFactory.create(IMG1)));
+            doc.add(new Image(ImageDataFactory.create(IMG2)));
+
+        } catch (FileNotFoundException | MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void manipulatePdf(List<String> images) {
+        Document doc = null;
+        try (PdfDocument pdfDoc = new PdfDocument(new PdfWriter(DEST))) {
+            doc = new Document(pdfDoc);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (String img : images) {
+            try {
+                if (doc != null) {
+                    doc.add(new Image(ImageDataFactory.create(img)));
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    private void rotateImage(String img) {
+        // TODO: 4/1/2019
+        //  All Image should be vertical
+    }
+
+    private void scaleImage(String img){
+        // TODO: 4/1/2019 scale images to be same size
+    }
+
+
 
 }
