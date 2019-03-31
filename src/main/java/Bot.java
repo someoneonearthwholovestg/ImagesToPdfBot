cd753044ddcca413dd9e19dff81531d88d4401ae
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-03-31 22:08:00 +0430

    token revoke

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,19 +1,34 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
-    private final static String token = "875607078:AAEY9iulL8uKhFANLXoRft5RLSeosAMWvAU";
+    private final static String TOKEN = "";
 
 
     public void onUpdateReceived(Update update) {
         System.out.println(update.getMessage());
-        update.getCallbackQuery();
+        SendMessage msg = new SendMessage(update.getMessage().getChatId(), "welcome!");
+
+        try {
+            System.out.println("UserName: " + getMe().getUserName());
+            System.out.println("FistName: " + getMe().getFirstName());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void onUpdatesReceived(List<Update> updates) {
-
+        try {
+            User me = getMe();
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getBotUsername() {
@@ -21,6 +36,6 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public String getBotToken() {
-        return token;
+        return TOKEN;
     }
 }
