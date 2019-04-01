ee3f0e4201d451400e36edbe096742d4a7372ced
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-04-01 17:25:38 +0430

    unit test added

--- src/test/java/PdfConverterTest.java
+++ src/test/java/PdfConverterTest.java
@@ -1,5 +1,19 @@
+import org.junit.Test;
+
+import java.nio.file.NotDirectoryException;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PdfConverterTest {
 
+    @Test
+    public void manipulatePdf() throws NotDirectoryException {
+        PdfConverter.manipulatePdf(PdfConverter.ORIGIN, PdfConverter.DEST);
+    }
+
+    @Test
+    public void manipulatePdf1() {
+        PdfConverter.manipulatePdf(List.of(PdfConverter.IMG1, PdfConverter.IMG2), PdfConverter.DEST);
+    }
 }
