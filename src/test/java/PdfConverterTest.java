bb18e3ddb4e2bd90156cc7eeb3c9491bafa580a4
morteza <mortezataqaddomi98@gmail.com>
2019-04-03 13:27:56 +0430

    testing new methods

--- src/test/java/PdfConverterTest.java
+++ src/test/java/PdfConverterTest.java
@@ -1,9 +1,8 @@
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 import java.nio.file.NotDirectoryException;
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 public class PdfConverterTest {
 
@@ -13,7 +12,14 @@ public class PdfConverterTest {
     }
 
     @Test
-    public void manipulatePdf1() {
-        PdfConverter.manipulatePdf(List.of(PdfConverter.IMG1, PdfConverter.IMG2), PdfConverter.DEST);
+    public void rotateImage() throws FileNotFoundException, MalformedURLException {
+        System.out.println(PdfConverter.ORIGIN + "/1.jpg");
+        PdfConverter.rotateImage(PdfConverter.ORIGIN + "/1.jpg", Rotate.RIGHT);
     }
+
+    @Test
+    public void addImageToPdf() throws MalformedURLException, FileNotFoundException {
+        PdfConverter.addImageToPdf(PdfConverter.ORIGIN + "/1.jpg", PdfConverter.DEST);
+    }
+
 }
